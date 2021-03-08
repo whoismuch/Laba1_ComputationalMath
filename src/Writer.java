23d3fb9126e2e2a5cf452f6ee431b0f046a3d0d5
whoismuch <bayramova2502@gmail.com>
2021-03-08 20:33:34 +0300

    little Fix

--- src/Writer.java
+++ src/Writer.java
@@ -18,21 +18,20 @@ public class Writer {
         DecimalFormat dF = new DecimalFormat("#.###");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
-                printValue("%15.3f", null, matrix[i][j]);
+                printValue("%21.3f", null, matrix[i][j]);
             }
-            printValue("%15.3f", null, b[i]);
+            printValue("%21.3f", null, b[i]);
             System.out.println("\r");
         }
     }
 
-    public void printVector (String message, String letter, double[] vector) {
+    public void printVector (String format, String message, String letter, double[] vector) {
         System.out.println(message);
         int count = 0;
         for (int i = 0; i < vector.length; i++) {
             count++;
             System.out.print(letter + count + " = ");
-            vector[i] = Math.round(vector[i] * 1000.0) / 1000.0;
-            printValue("%1.3f", null, vector[i]);
+            printValue(format, null, vector[i]);
             System.out.println( );
         }
     }
